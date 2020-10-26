e46f4762a1d9588685c0a9c3741d34b74cda7173
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2020-10-26 09:22:24 +0100

    Add files via upload

---
+++ Varijable1/Varijable1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena;
+            float BrojOcjena = 0;
+            float ZbrojOcjena = 0;
+            string uputa = "Upisi ocjenu:";
+            string ispis = "Prosjek ocjena je:";
+            float prosjek;
+
+            do
+            {
+                Console.Write(uputa);
+                ocjena = Convert.ToInt32(Console.ReadLine());
+                if (ocjena >= 1 && ocjena <= 5)
+                {
+                    BrojOcjena++;
+                    ZbrojOcjena += ocjena;
+
+                }
+                else if (ocjena == 0)
+                {
+                    prosjek = ZbrojOcjena / BrojOcjena;
+                    Console.Write(ispis);
+                    Console.WriteLine(prosjek);
+
+                }
+                else
+                {
+                    Console.WriteLine("Molimo unesite ispravnu ocjenu (1-5)!");
+                }
+            }
+            while (ocjena != 0);
+
+            Console.ReadKey();
+        }
+    }
+}
